9b1d948a6f7c8be342a31a93dccd5ffac9caef32
PALAFOX955 <normagarcia04rg@gmail.com>
2025-06-24 15:16:18 -0600

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import server from "./server";
 import { Request, Response, NextFunction } from "express";
-
+const PORT = process.env.PORT || 3000;
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
     console.log('http://localhost:3000');
